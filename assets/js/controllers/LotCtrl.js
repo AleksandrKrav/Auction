9f17a766a22efce24024fafa3964e47006bf3c25
PetroOv <petroov@gmail.com>
2017-01-06 19:20:47 +0200

    Add dashboard view design

--- assets/js/controllers/LotCtrl.js
+++ assets/js/controllers/LotCtrl.js
@@ -5,7 +5,7 @@
 'use strict';
 auApp.controller('LotCtrl', ['$scope', '$routeParams', 'LotService', 'UserService',
   function ($scope, $routeParams, LotService, UserService) {
-    debugger;
+    // debugger;
     var userID = $routeParams.id;
     $scope.showForm = false;
     $scope.lots = [];
